15a78a258e14cb7ddb27cad5b5dd20e61cf80fe7
tom <tom@f6fd3633-1913-47c2-bad4-372d75c948ec>
2006-07-17 15:40:39 +0000

    Manually add '' to tag file.
    git-svn-id: file:///Users/tom/Code/svn/repos/Astro-Coord-ECI/trunk@346 f6fd3633-1913-47c2-bad4-372d75c948ec

--- t/Ctags.pm
+++ t/Ctags.pm
@@ -2,6 +2,45 @@ package t::Ctags;
 
 our $VERSION = '0.001';
 
+use ExtUtils::MakeMaker;
+
+sub make_tags {
+    my $fh;
+    open ($fh, '<', 'MANIFEST') or die <<eod;
+Error - Failed to open MANIFEST.
+        $!
+eod
+    my @files;
+    while (<$fh>) {
+	chomp;
+	s/\s+.*//;
+	m/\.pm$/i || m|bin/|i or next;
+	push @files, $_;
+    }
+    system ("ctags @files");
+    $? and die <<eod;
+Error - ctags command failed. Status = $?
+eod
+
+    my @tags;
+    open ($fh, '<', 'tags') or die <<eod;
+Error - Failed to open tags for input.
+        $!
+eod
+    while (<$fh>) {push @tags, $_}
+    foreach my $fn (@files) {
+	my $version = MM->parse_version ($fn);
+	next unless defined $version && $version ne 'undef';
+	push @tags, "\$VERSION\t$fn\t" . '/\m^\s*our\s\s*\$VERSION\s*=/;"' .
+		"\tv\n";
+    }
+    open ($fh, '>', 'tags') or die <<eod;
+Error - Failed to open tags for output.
+        $!
+eod
+    foreach (sort @tags) {print $fh $_}
+}
+
 package MY;
 
 my $ctags = `which ctags`;
@@ -11,10 +50,10 @@ sub top_targets {
     my $target = $self->SUPER::top_targets (@_);
     if ($ctags) {
 	$target =~ s/$/ tags/m;
-	$target .= <<eod;
+	$target .= <<'eod';
 
-tags :: \$(MAN1PODS) \$(MAN3PODS)
-\tctags `grep .pm MANIFEST && grep bin/ MANIFEST`
+tags :: $(MAN1PODS) $(MAN3PODS)
+	perl -Mt::Ctags -e 't::Ctags->make_tags'
 
 eod
     }
