fc31392a0505206d4ac6a552f9e0bd7ebe6cb2c5
tom <tom@f6fd3633-1913-47c2-bad4-372d75c948ec>
2008-12-27 20:38:54 +0000

    Add -end effector, to end the test before the end of the test specification
    git-svn-id: file:///Users/tom/Code/svn/repos/Astro-Coord-ECI/trunk@1174 f6fd3633-1913-47c2-bad4-372d75c948ec

--- t/Satpass.pm
+++ t/Satpass.pm
@@ -10,7 +10,7 @@ use Cwd;
 use File::Spec;
 use Test;
 
-our $VERSION = '0.003_01';
+our $VERSION = '0.003_02';
 
 #	We may need IO::String for the test. If we do, make sure it
 #	is available. If it is not, skip everything.
@@ -39,50 +39,55 @@ my @todo = ();		# Tests expected to fail.
 my %h_todo;		# Hash of tests expected to fail.
 
 sub satpass {
-my $handle = shift;
+    my $handle = shift;
 
 #	If we can not get off the ground, do not try.
 
-if ($gblskip) {
-    plan tests => 1;
-    skip ($gblskip, 1);
-    return;
-}
+    if ($gblskip) {
+	plan tests => 1;
+	skip ($gblskip, 1);
+	return;
+    }
 
-$| = 1;
+    $| = 1;
 
 #	Set up the testing hook in satpass.
 # >>>	This interface is undocumented, and unsupported except for its
 # >>>	use in this test script.
 
-no warnings qw{once};
-$Astro::satpass::Test::Hook = \&tester;
-$Astro::satpass::Test::Handle = $handle;
-use warnings qw{once};
+    no warnings qw{once};
+    $Astro::satpass::Test::Hook = \&tester;
+    $Astro::satpass::Test::Handle = $handle;
+    use warnings qw{once};
 
 #	Make a pass through the <DATA> to figure out how many tests
 #	there are. Tell the Test package how many.
 
-my $start = tell ($handle);
-while (<$handle>) {
-    if (m/^\s*-test\b/) {$test++}
-      elsif (m/^\s*-todo\b/) {push @todo, $test}
+    my $start = tell ($handle);
+    while (<$handle>) {
+	if (m/^\s*-test\b/) {
+	    $test++
+	} elsif (m/^\s*-todo\b/) {
+	    push @todo, $test;
+	} elsif (m/^\s*-end\b/) {
+	    last;
+	}
     }
-seek ($handle, $start, 0);
-plan tests => $test, todo => \@todo;
-%h_todo = map {$_ => 1} @todo;
+    seek ($handle, $start, 0);
+    plan tests => $test, todo => \@todo;
+    %h_todo = map {$_ => 1} @todo;
 
 #	We start from test 1 (since we increment before use).
 
-$test = 0;
+    $test = 0;
 
 #	Set up the command arguments and 'do' the satpass script. All
 #	further work is done by tester() when the script calls it.
 
-local @ARGV = ('-filter', -initialization_file => File::Spec->devnull ());
-$skip = '';
-do $script;
-print $@ if $@;
+    local @ARGV = ('-filter', -initialization_file => File::Spec->devnull ());
+    $skip = '';
+    do $script;
+    print $@ if $@;
 }
 
 #	not_available(module ...) is a utility to determine whether the
@@ -90,15 +95,15 @@ print $@ if $@;
 #	returns a message for the first module that can not be loaded.
 
 sub not_available {
-foreach my $module (@_) {
-    # Perl::Critic wants us not to do this, but using 'require $fn'
-    # requires us to duplicate the bareword logic. That was not too
-    # bad when .pm was the only option for the file type, but when
-    # they added .pmc things got complex, and they might change again.
-    eval "require $module";	## no critic
-    return "Module $module can not be loaded." if $@;
+    foreach my $module (@_) {
+	# Perl::Critic wants us not to do this, but using 'require $fn'
+	# requires us to duplicate the bareword logic. That was not too
+	# bad when .pm was the only option for the file type, but when
+	# they added .pmc things got complex, and they might change again.
+	eval "require $module";	## no critic
+	return "Module $module can not be loaded." if $@;
     }
-return '';
+    return '';
 }
 
 #	not_reachable($url ...) is a utilty to determine whether the given
@@ -107,14 +112,15 @@ return '';
 #	the results of not_available ('LWP::UserAgent').
 
 sub not_reachable {
-my $ok = not_available ('LWP::UserAgent');
-return $ok if $ok;
-my $ua = LWP::UserAgent->new () or return "Cannot instantiate LWP::UserAgent.\n$@";
-foreach my $url (@_) {
-    my $resp = $ua->get ($url);
-    return $resp->status_line unless $resp->is_success;
+    my $ok = not_available ('LWP::UserAgent');
+    return $ok if $ok;
+    my $ua = LWP::UserAgent->new ()
+	or return "Cannot instantiate LWP::UserAgent.\n$@";
+    foreach my $url (@_) {
+	my $resp = $ua->get ($url);
+	return $resp->status_line unless $resp->is_success;
     }
-return '';
+    return '';
 }
 
 #	tester() is the test callback. It is called whenever the
@@ -147,116 +153,125 @@ return '';
 #	otherwise they are true if $output eq $data.
 
 sub tester {
-my ($handle, $input, $output, $except) = @_;
-##print "foo> $input" if $input;
-##print $output if $output;
-while (<$handle>) {
-    chomp;
-    s/^\s+//;
-    s/\s+$//;
-    next unless $_;
-    next if m/^#/;
-    unless (m/^-/) {
-	next if $skip;
-	return "$_\n";
+    my ($handle, $input, $output, $except) = @_;
+##  print "foo> $input" if $input;
+##  print $output if $output;
+    while (<$handle>) {
+	chomp;
+	s/^\s+//;
+	s/\s+$//;
+	next unless $_;
+	next if m/^#/;
+	unless (m/^-/) {
+	    next if $skip;
+	    return "$_\n";
 	}
 
 #	We support here documents in directives. The syntax is
 #	pretty much the same as Perl's, except the indicator may
 #	not be quoted, and we don't do interpolation.
 
-    s|<<(.*)|| and do {
-	my $flag = ($1 || '') . "\n";
-	my $buffer = $_ . "\n";
-	while (<$handle>) {
-	    $_ eq $flag and do {$flag = undef; last};
-	    $buffer .= $_;
-	    }
-	die <<eod if defined $flag;
+	s|<<(.*)|| and do {
+	    my $flag = ($1 || '') . "\n";
+	    my $buffer = $_ . "\n";
+	    while (<$handle>) {
+		$_ eq $flag and do {$flag = undef; last};
+		$buffer .= $_;
+		}
+	    die <<eod if defined $flag;
 Error - Failed to find end of here document '<<$flag' before EOF.
 eod
-	$_ = $buffer;
+	    $_ = $buffer;
 	};
 
 #	-data loads the rest of the text into  $data.
 
-    s/-data\b\s*//m and do {$data = $_; next};
+	s/-data\b\s*//m and do {$data = $_; next};
+
+#	-end ends the script early.
+
+	s/-end\b// and last;
 
 #	-fail specifies a note to be output if the next test fails.
 
-    s/-fail\b\s*//m and do {$failure = $_; next};
+	s/-fail\b\s*//m and do {$failure = $_; next};
 
 #	-home sets us back to the directory we started in.
 
-    s/-home\b\s*//m and do {chdir $home; next};
+	s/-home\b\s*//m and do {chdir $home; next};
 
 #	-read reads the named file into $output. The presumption is
 #	that we're testing output redirected to a file.
 
-    s/-read\b\s*//m and do {
-	my $fh;
-	open ($fh, '<', File::Spec->catfile (cwd, $_));
-	local $/ = undef;
-	$output = <$fh>;
-	next;
+	s/-read\b\s*//m and do {
+	    my $fh;
+	    open ($fh, '<', File::Spec->catfile (cwd, $_));
+	    local $/ = undef;
+	    $output = <$fh>;
+	    next;
 	};
 
 #	-result evals the rest of the line, placing the output into
 #	$output. The presumption is that we're doing some computation
 #	to determine the actual results of the test.
 
-    s/-result\b\s*//m and do {
-	# Perl::Critic does not like string evals, but it's the only
-	# way to execute arbitrary code out of the data.
-	$output = eval $_;	## no critic
-	die $@ if $@; next};
+	s/-result\b\s*//m and do {
+	    # Perl::Critic does not like string evals, but it's the only
+	    # way to execute arbitrary code out of the data.
+	    $output = eval $_;	## no critic
+	    die $@ if $@; next
+	};
 
 #	-skip evals the rest of the line, placing the output into the
 #	$skip variable. This will _not_ override any global
 #	considerations that force the whole shebang to be skipped.
 
-    s/-skip\b\s*//m and do {
-	# Perl::Critic does not like string evals, but it's the only
-	# way to execute arbitrary code out of the data.
-	$skip = eval $_;	## no critic
-	die $@ if $@; next};
+	s/-skip\b\s*//m and do {
+	    # Perl::Critic does not like string evals, but it's the only
+	    # way to execute arbitrary code out of the data.
+	    $skip = eval $_;	## no critic
+	    die $@ if $@; next
+	};
 
 #	-test actually performs the test. The rest of the line is an
 #	optional title for the test. Note that if $except is defined,
 #	it becomes the thing we test.
 
-    s/-test\b\s*//m and do {
-	$test++;
-	print "#\n";
-	print $_ ? "# Test $test - $_\n" : "# Test $test\n";
-##	$data =~ s/^\s*\n//m;
-	chomp $data;
-	print $data !~ m/\n/g ?
-	    "#      Expected: $data\n" :
-	   ("#      Expected:\n", map {"#         $_\n"} split '\n', $data);
-	$output = $except if defined $except;
-	$output =~ s/^\s*\n//m;
-	chomp $output;
-	print $output !~ m/\n/g ?
-	    "#           Got: $output\n" :
-	   ("#           Got:\n", map {"#         $_\n"} split '\n', $output);
-	skip ($skip, $data eq $output);
-	warn sprintf "\n\n$failure\n\n", $test
-	    unless $skip || $h_todo{$test} || $data eq $output || !$failure;
-	$failure = undef;
-	next;
+	s/-test\b\s*//m and do {
+	    $test++;
+	    print "#\n";
+	    print $_ ? "# Test $test - $_\n" : "# Test $test\n";
+##	    $data =~ s/^\s*\n//m;
+	    chomp $data;
+	    print $data !~ m/\n/g ?
+		"#      Expected: $data\n" :
+	       ("#      Expected:\n", map {"#         $_\n"} split '\n', $data);
+	    $output = $except if defined $except;
+	    $output =~ s/^\s*\n//m;
+	    chomp $output;
+	    print $output !~ m/\n/g ?
+		"#           Got: $output\n" :
+	       ("#           Got:\n", map {"#         $_\n"} split '\n', $output);
+	    skip ($skip, $data eq $output);
+	    warn sprintf "\n\n$failure\n\n", $test
+		unless $skip || $h_todo{$test} || $data eq $output || !$failure;
+	    $failure = undef;
+	    next;
 	};
 
 #	-todo tells the tester that the PREVIOUS -test is expected to
 #	fail. It is no-opped here, because we took care of it in the
 #	first pass through the test script.
 
-    s/-todo\b\s*// and next;
+	s/-todo\b\s*// and next;
 
 #	-unlink unlinks the named file. It would be nice if there were
 #	automatic cleanup, but there is not.
 
-    s/-unlink\b\s*//m and do {unlink File::Spec->catfile (cwd, $_); next};
+	s/-unlink\b\s*//m and do {
+	    unlink File::Spec->catfile (cwd, $_);
+	    next
+	};
 
 #	-write writes the content of $data to the named file. It's
 #	$data because I figured it could always be reset before the
@@ -265,16 +280,16 @@ eod
 #	command) the text written to the file was also what I wanted
 #	to test the results against, but I can't say I planned that.
 
-    s/-write\b\s*//m and do {
-	my $fh;
-	open ($fh, '>', File::Spec->catfile (getcwd, $_));
-	print $fh $data;
-	next;
+	s/-write\b\s*//m and do {
+	    my $fh;
+	    open ($fh, '>', File::Spec->catfile (getcwd, $_));
+	    print $fh $data;
+	    next;
 	};
 
 #	If we get here, die complaining of an unknown directive.
 
-    die <<eod;
+	die <<eod;
 Error - Unknown directive: $_
 eod
 
@@ -283,7 +298,7 @@ eod
 #	If we run out of <DATA>, we return undef to request the script
 #	to exit.
 
-return;
+    return;
 }
 
 1;
