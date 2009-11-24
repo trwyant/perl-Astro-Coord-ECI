1f1ad23a4d8f55f243bf3b4533cb372b8f1cc04a
tom <tom@f6fd3633-1913-47c2-bad4-372d75c948ec>
2009-11-24 19:39:00 +0000

    Change to the CPAN-recommended license. This means adding a LICENSES diretory containing files Artistic (the Artistic License) and Copying (the GNU GPL), and replacing the LICENSE and COPYRIGHT sections in all code with the recommended LICENSE AND COPYRIGHT section.
    git-svn-id: file:///Users/tom/Code/svn/repos/Astro-Coord-ECI/trunk@1506 f6fd3633-1913-47c2-bad4-372d75c948ec

--- lib/Astro/Coord/ECI/TLE/Iridium.pm
+++ lib/Astro/Coord/ECI/TLE/Iridium.pm
@@ -1644,20 +1644,17 @@ L<http://rt.cpan.org/>.
 
 Thomas R. Wyant, III (F<wyant at cpan dot org>)
 
-=head1 COPYRIGHT
+=head1 COPYRIGHT AND LICENSE
 
-Copyright 2006, 2007, 2008, 2009 by Thomas R. Wyant, III
-(F<wyant at cpan dot org>). All rights reserved.
+Copyright (C) 2006-2009, Thomas R. Wyant, III
 
-=head1 LICENSE
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl 5.10.0. For more details, see the full text
+of the licenses in the directory LICENSES.
 
-This module is free software; you can use it, redistribute it and/or
-modify it under the same terms as Perl itself. Please see
-L<http://perldoc.perl.org/index-licence.html> for the current licenses.
-
-This software is provided without any warranty of any kind, express or
-implied. The author will not be liable for any damages of any sort
-relating in any way to this software.
+This program is distributed in the hope that it will be useful, but
+without any warranty; without even the implied warranty of
+merchantability or fitness for a particular purpose.
 
 =cut
 
