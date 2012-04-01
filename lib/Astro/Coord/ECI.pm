6b0717d53ed1e549d60525b0e1cc6fa36846e7d2
tom <tom@f6fd3633-1913-47c2-bad4-372d75c948ec>
2012-04-01 02:35:27 +0000

    Update version to 0.049_01. Update Changes file for changes since 0.049.
    git-svn-id: file:///Users/tom/Code/svn/repos/Astro-Coord-ECI/trunk@3207 f6fd3633-1913-47c2-bad4-372d75c948ec

--- lib/Astro/Coord/ECI.pm
+++ lib/Astro/Coord/ECI.pm
@@ -131,7 +131,7 @@ package Astro::Coord::ECI;
 use strict;
 use warnings;
 
-our $VERSION = '0.049';
+our $VERSION = '0.049_01';
 
 use Astro::Coord::ECI::Utils qw{:all};
 use Carp;
--- lib/Astro/Coord/ECI/Utils.pm
+++ lib/Astro/Coord/ECI/Utils.pm
@@ -104,7 +104,7 @@ package Astro::Coord::ECI::Utils;
 use strict;
 use warnings;
 
-our $VERSION = '0.049';
+our $VERSION = '0.049_01';
 our @ISA = qw{Exporter};
 
 use Carp;
