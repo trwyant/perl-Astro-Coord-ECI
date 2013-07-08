3dbfdee9bcda74b8c0b1edd5d1b506b9dc9f71dc
tom <tom@f6fd3633-1913-47c2-bad4-372d75c948ec>
2013-07-08 22:10:39 +0000

    Set version to 0.057. Update Changes file for changes since 0.056_04.
    git-svn-id: file:///Users/tom/Code/svn/repos/Astro-Coord-ECI/trunk@3956 f6fd3633-1913-47c2-bad4-372d75c948ec

--- inc/Astro/Coord/ECI/Test.pm
+++ inc/Astro/Coord/ECI/Test.pm
@@ -5,7 +5,7 @@ use 5.006002;
 use strict;
 use warnings;
 
-our $VERSION = '0.056_04';
+our $VERSION = '0.057';
 
 use base qw{ Exporter };
 
--- lib/Astro/Coord/ECI.pm
+++ lib/Astro/Coord/ECI.pm
@@ -143,7 +143,7 @@ package Astro::Coord::ECI;
 use strict;
 use warnings;
 
-our $VERSION = '0.056_04';
+our $VERSION = '0.057';
 
 use Astro::Coord::ECI::Utils qw{:all};
 use Carp;
