56ac8286ee6e8d3a00fc19e4d54a9c612847e845
Tom Wyant <wyant@cpan.org>
2014-03-22 11:20:21 -0400

    Tweak fold_case() implementation for Perl < 5.014.

--- lib/Astro/Coord/ECI/Utils.pm
+++ lib/Astro/Coord/ECI/Utils.pm
@@ -587,7 +587,7 @@ C<CORE::fc> if that is available, otherwise it maps to C<CORE::lc>.
 
 =cut
 
-*fold_case = CORE->can( 'fc' ) || CORE->can( 'lc' );
+*fold_case = CORE->can( 'fc' ) || sub ($) { return lc $_[0] };
 
 
 =item $difference = intensity_to_magnitude ($ratio)
