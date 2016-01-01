8cda8eb9d6507be7c77c970f00a538be4f85e198
Tom Wyant <wyant@cpan.org>
2016-01-01 12:21:22 -0500

    Add GitHub repository to metadata.

--- inc/Astro/Coord/ECI/Meta.pm
+++ inc/Astro/Coord/ECI/Meta.pm
@@ -28,6 +28,27 @@ sub distribution {
     return $self->{distribution};
 }
 
+sub meta_merge {
+    return {
+	'meta-spec'	=> {
+	    version	=> 2,
+	},
+	dynamic_config	=> 1,
+	no_index	=> {
+	    directory	=> [ qw{ inc t xt } ],
+	},
+	resources	=> {
+	    bugtracker	=> 'https://rt.cpan.org/Public/Dist/Display.html?Name=Astro-satpass',
+	    license	=> 'http://dev.perl.org/licenses/',
+	    repository	=> {
+		type	=> 'git',
+		url	=> 'git://github.com/trwyant/perl-Astro-Coord-ECI.git',
+		web	=> 'https://github.com/trwyant/perl-Astro-Coord-ECI',
+	    },
+	}
+    };
+}
+
 sub requires {
     my ( $self, @extra ) = @_;
 ##  if ( ! $self->distribution() ) {
@@ -105,6 +126,16 @@ C<< {META_MERGE}->{build_requires} >> key.
 
 This method returns the value of the environment variable
 C<MAKING_MODULE_DISTRIBUTION> at the time the object was instantiated.
+
+=head2 meta_merge
+
+ use YAML;
+ print Dump( $meta->meta_merge() );
+
+This method returns a reference to a hash describing the meta-data which
+has to be provided by making use of the builder's C<meta_merge>
+functionality. This includes the C<dynamic_config>, C<no_index> and
+C<resources> data.
 
 =head2 requires
 
