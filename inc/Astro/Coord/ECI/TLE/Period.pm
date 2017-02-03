d0bc753da16c6a3c0a106f41d461af6f0a153760
Tom Wyant <wyant@cpan.org>
2017-02-03 12:07:38 -0500

    Version 0.077_02
    Set version to 0.077_02. Update Changes file for changes since v00.077_01.

--- inc/Astro/Coord/ECI/TLE/Period.pm
+++ inc/Astro/Coord/ECI/TLE/Period.pm
@@ -9,7 +9,7 @@ use Astro::Coord::ECI::TLE;
 use Carp;
 
 our @ISA = qw{ Astro::Coord::ECI::TLE };
-our $VERSION = '0.077_01';
+our $VERSION = '0.077_02';
 
 my $pkg = __PACKAGE__;
 
--- lib/Astro/Coord/ECI/TLE/Iridium.pm
+++ lib/Astro/Coord/ECI/TLE/Iridium.pm
@@ -122,7 +122,7 @@ use warnings;
 
 use base qw{Astro::Coord::ECI::TLE};
 
-our $VERSION = '0.077_01';
+our $VERSION = '0.077_02';
 
 use Astro::Coord::ECI::Sun;
 use Astro::Coord::ECI::Utils qw{:all};
