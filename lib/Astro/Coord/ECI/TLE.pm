44f4336e6c835eb8c36e9f054c8524fa2f0d0762
Tom Wyant <wyant@cpan.org>
2018-02-02 08:45:18 -0500

    Version 0.089
    Set version to 0.089. Update Changes file for changes since v00.088_03.

--- lib/Astro/Coord/ECI/TLE.pm
+++ lib/Astro/Coord/ECI/TLE.pm
@@ -231,7 +231,7 @@ package Astro::Coord::ECI::TLE;
 use strict;
 use warnings;
 
-our $VERSION = '0.088_03';
+our $VERSION = '0.089';
 
 use base qw{ Astro::Coord::ECI Exporter };
 
--- lib/Astro/Coord/ECI/Utils.pm
+++ lib/Astro/Coord/ECI/Utils.pm
@@ -104,7 +104,7 @@ package Astro::Coord::ECI::Utils;
 use strict;
 use warnings;
 
-our $VERSION = '0.088_03';
+our $VERSION = '0.089';
 our @ISA = qw{Exporter};
 
 use Carp;
