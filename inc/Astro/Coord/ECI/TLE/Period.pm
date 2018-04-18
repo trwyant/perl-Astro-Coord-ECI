ce7d055f3d419d08e5ffff058e1b7f244feec98d
Tom Wyant <wyant@cpan.org>
2018-04-18 07:14:27 -0400

    Version 0.093_03
    Set version to 0.093_03. Update Changes file for changes since v00.093_02.

--- inc/Astro/Coord/ECI/TLE/Period.pm
+++ inc/Astro/Coord/ECI/TLE/Period.pm
@@ -9,7 +9,7 @@ use Astro::Coord::ECI::TLE;
 use Carp;
 
 our @ISA = qw{ Astro::Coord::ECI::TLE };
-our $VERSION = '0.093_02';
+our $VERSION = '0.093_03';
 
 my $pkg = __PACKAGE__;
 
--- lib/Astro/Coord/ECI/Utils.pm
+++ lib/Astro/Coord/ECI/Utils.pm
@@ -113,7 +113,7 @@ package Astro::Coord::ECI::Utils;
 use strict;
 use warnings;
 
-our $VERSION = '0.093_02';
+our $VERSION = '0.093_03';
 our @ISA = qw{Exporter};
 
 use Carp;
