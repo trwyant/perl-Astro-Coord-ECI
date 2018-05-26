299632432d1baa6af7706af7261efc59abaa7325
Tom Wyant <wyant@cpan.org>
2018-05-26 06:48:43 -0400

    Version 0.096_01
    Set version to 0.096_01. Update Changes file for changes since v00.096.

--- inc/My/Module/Test.pm
+++ inc/My/Module/Test.pm
@@ -5,7 +5,7 @@ use 5.006002;
 use strict;
 use warnings;
 
-our $VERSION = '0.096';
+our $VERSION = '0.096_01';
 
 use Exporter qw{ import };
 
--- lib/Astro/Coord/ECI/Star.pm
+++ lib/Astro/Coord/ECI/Star.pm
@@ -50,7 +50,7 @@ package Astro::Coord::ECI::Star;
 use strict;
 use warnings;
 
-our $VERSION = '0.096';
+our $VERSION = '0.096_01';
 
 use base qw{Astro::Coord::ECI};
 
