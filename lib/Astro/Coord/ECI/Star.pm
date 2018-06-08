5c8d5e1c1963e60733e0672d4035cf64dd810117
Tom Wyant <wyant@cpan.org>
2018-06-08 09:33:50 -0400

    Version 0.097_01
    Set version to 0.097_01. Update Changes file for changes since v00.097.

--- lib/Astro/Coord/ECI/Star.pm
+++ lib/Astro/Coord/ECI/Star.pm
@@ -50,7 +50,7 @@ package Astro::Coord::ECI::Star;
 use strict;
 use warnings;
 
-our $VERSION = '0.097';
+our $VERSION = '0.097_01';
 
 use base qw{Astro::Coord::ECI};
 
--- lib/Astro/Coord/ECI/TLE/Iridium.pm
+++ lib/Astro/Coord/ECI/TLE/Iridium.pm
@@ -137,7 +137,7 @@ use warnings;
 
 use base qw{Astro::Coord::ECI::TLE};
 
-our $VERSION = '0.097';
+our $VERSION = '0.097_01';
 
 use Astro::Coord::ECI::Sun;
 use Astro::Coord::ECI::Utils qw{:all};
