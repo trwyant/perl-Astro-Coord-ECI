4cf7a48be5df12e61b9ccbdda4ccd76454b31b4a
Tom Wyant <wyant@cpan.org>
2019-08-22 18:51:27 -0400

    Version 0.107_01
    Set version to 0.107_01. Update Changes file for changes since v00.107.

--- lib/Astro/Coord/ECI/Mixin.pm
+++ lib/Astro/Coord/ECI/Mixin.pm
@@ -14,7 +14,7 @@ use Astro::Coord::ECI::Utils qw{ __default_station
 use Exporter ();
 use POSIX qw{ floor };
 
-our $VERSION = '0.107';
+our $VERSION = '0.107_01';
 
 our @EXPORT_OK = qw{
     almanac almanac_hash
