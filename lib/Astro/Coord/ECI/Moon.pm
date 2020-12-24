3bc9a930255fca732e3b8aca1aff1707a90b6d49
Tom Wyant <wyant@cpan.org>
2020-12-24 11:23:01 -0500

    Version 0.116_01
    Set version to 0.116_01. Update Changes file for changes since v00.116.

--- lib/Astro/Coord/ECI/Moon.pm
+++ lib/Astro/Coord/ECI/Moon.pm
@@ -44,7 +44,7 @@ package Astro::Coord::ECI::Moon;
 use strict;
 use warnings;
 
-our $VERSION = '0.116';
+our $VERSION = '0.116_01';
 
 use base qw{Astro::Coord::ECI};
 
--- lib/Astro/Coord/ECI/Utils.pm
+++ lib/Astro/Coord/ECI/Utils.pm
@@ -123,7 +123,7 @@ package Astro::Coord::ECI::Utils;
 use strict;
 use warnings;
 
-our $VERSION = '0.116';
+our $VERSION = '0.116_01';
 our @ISA = qw{Exporter};
 
 use Carp;
