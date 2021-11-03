282cb4e1d7981dafb9efa5399484f234d462fd81
Tom Wyant <wyant@cpan.org>
2021-11-02 23:18:16 -0400

    Version 0.121_01
    Set version to 0.121_01. Update Changes file for changes since v00.121.

--- lib/Astro/Coord/ECI.pm
+++ lib/Astro/Coord/ECI.pm
@@ -92,7 +92,7 @@ package Astro::Coord::ECI;
 use strict;
 use warnings;
 
-our $VERSION = '0.121';
+our $VERSION = '0.121_01';
 
 use Astro::Coord::ECI::Utils qw{ @CARP_NOT :mainstream };
 use Carp;
--- lib/Astro/Coord/ECI/Star.pm
+++ lib/Astro/Coord/ECI/Star.pm
@@ -50,7 +50,7 @@ package Astro::Coord::ECI::Star;
 use strict;
 use warnings;
 
-our $VERSION = '0.121';
+our $VERSION = '0.121_01';
 
 use base qw{Astro::Coord::ECI};
 
