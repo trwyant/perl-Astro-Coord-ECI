568101f17d96c9f998d71a0bd35beb34ffa2e7b1
Tom Wyant <wyant@cpan.org>
2022-05-21 09:01:14 -0400

    Version 0.124
    Set version to 0.124. Update Changes file for changes since v00.123_01.

--- lib/Astro/Coord/ECI/Mixin.pm
+++ lib/Astro/Coord/ECI/Mixin.pm
@@ -14,7 +14,7 @@ use Astro::Coord::ECI::Utils qw{ __default_station
 use Exporter ();
 use POSIX qw{ floor };
 
-our $VERSION = '0.123_01';
+our $VERSION = '0.124';
 
 our @EXPORT_OK = qw{
     almanac almanac_hash
--- lib/Astro/Coord/ECI/Sun.pm
+++ lib/Astro/Coord/ECI/Sun.pm
@@ -49,7 +49,7 @@ package Astro::Coord::ECI::Sun;
 use strict;
 use warnings;
 
-our $VERSION = '0.123_01';
+our $VERSION = '0.124';
 
 use base qw{Astro::Coord::ECI};
 
