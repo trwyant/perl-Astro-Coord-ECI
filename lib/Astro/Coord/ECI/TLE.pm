6ece474307a885542d92c96f13394242afbfc254
Tom Wyant <wyant@cpan.org>
2022-06-07 14:05:18 -0400

    Version 0.124_01
    Set version to 0.124_01. Update Changes file for changes since v00.124.

--- lib/Astro/Coord/ECI/TLE.pm
+++ lib/Astro/Coord/ECI/TLE.pm
@@ -229,7 +229,7 @@ package Astro::Coord::ECI::TLE;
 use strict;
 use warnings;
 
-our $VERSION = '0.124';
+our $VERSION = '0.124_01';
 
 use base qw{ Astro::Coord::ECI Exporter };
 
--- lib/Astro/Coord/ECI/TLE/Set.pm
+++ lib/Astro/Coord/ECI/TLE/Set.pm
@@ -143,7 +143,7 @@ our @CARP_NOT = qw{
     Astro::Coord::ECI
 };
 
-our $VERSION = '0.124';
+our $VERSION = '0.124_01';
 
 use constant ERR_NOCURRENT => <<eod;
 Error - Can not call %s because there is no current member. Be
