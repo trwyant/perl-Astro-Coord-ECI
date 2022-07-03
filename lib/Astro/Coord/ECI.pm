cdeb8cb141fe570f2a02757a409323031ad18198
Tom Wyant <wyant@cpan.org>
2022-07-03 08:33:04 -0400

    Version 0.126
    Set version to 0.126. Update Changes file for changes since v00.125_01.

--- lib/Astro/Coord/ECI.pm
+++ lib/Astro/Coord/ECI.pm
@@ -92,7 +92,7 @@ package Astro::Coord::ECI;
 use strict;
 use warnings;
 
-our $VERSION = '0.125_01';
+our $VERSION = '0.126';
 
 use Astro::Coord::ECI::Utils qw{ @CARP_NOT :mainstream };
 use Carp;
--- lib/Astro/Coord/ECI/TLE.pm
+++ lib/Astro/Coord/ECI/TLE.pm
@@ -229,7 +229,7 @@ package Astro::Coord::ECI::TLE;
 use strict;
 use warnings;
 
-our $VERSION = '0.125_01';
+our $VERSION = '0.126';
 
 use base qw{ Astro::Coord::ECI Exporter };
 
