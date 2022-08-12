415e688e0e4beb550f36b38fdea49595fd0b55d2
Tom Wyant <wyant@cpan.org>
2022-08-12 11:59:19 -0400

    Version 0.127
    Set version to 0.127. Update Changes file for changes since v00.126_01.

--- inc/My/Module/Sun.pm
+++ inc/My/Module/Sun.pm
@@ -9,7 +9,7 @@ use base qw{ Astro::Coord::ECI::Sun };
 
 use Carp;
 
-our $VERSION = '0.126_01';
+our $VERSION = '0.127';
 
 1;
 
--- lib/Astro/Coord/ECI/TLE.pm
+++ lib/Astro/Coord/ECI/TLE.pm
@@ -229,7 +229,7 @@ package Astro::Coord::ECI::TLE;
 use strict;
 use warnings;
 
-our $VERSION = '0.126_01';
+our $VERSION = '0.127';
 
 use base qw{ Astro::Coord::ECI Exporter };
 
