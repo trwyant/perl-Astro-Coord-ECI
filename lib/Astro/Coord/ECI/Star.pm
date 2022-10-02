a75a98d508db4dafba35b6519e52b318fb73e171
Tom Wyant <wyant@cpan.org>
2022-10-02 08:54:22 -0400

    Version 0.128
    Set version to 0.128. Update Changes file for changes since v00.127_01.

--- lib/Astro/Coord/ECI/Star.pm
+++ lib/Astro/Coord/ECI/Star.pm
@@ -50,7 +50,7 @@ package Astro::Coord::ECI::Star;
 use strict;
 use warnings;
 
-our $VERSION = '0.127_01';
+our $VERSION = '0.128';
 
 use base qw{Astro::Coord::ECI};
 
--- lib/Astro/Coord/ECI/Sun.pm
+++ lib/Astro/Coord/ECI/Sun.pm
@@ -49,7 +49,7 @@ package Astro::Coord::ECI::Sun;
 use strict;
 use warnings;
 
-our $VERSION = '0.127_01';
+our $VERSION = '0.128';
 
 use base qw{Astro::Coord::ECI};
 
