ed1906a1427132c2bd3029114567a8553510467c
Tom Wyant <harryfmudd@comcast.net>
2026-06-18 07:51:29 -0400

    Version 0.135
    Set version to 0.135. Update Changes file for changes since v00.134_01.

--- lib/Astro/Coord/ECI/Star.pm
+++ lib/Astro/Coord/ECI/Star.pm
@@ -50,7 +50,7 @@ package Astro::Coord::ECI::Star;
 use strict;
 use warnings;
 
-our $VERSION = '0.134_01';
+our $VERSION = '0.135';
 
 use base qw{Astro::Coord::ECI};
 
--- lib/Astro/Coord/ECI/TLE.pm
+++ lib/Astro/Coord/ECI/TLE.pm
@@ -229,7 +229,7 @@ package Astro::Coord::ECI::TLE;
 use strict;
 use warnings;
 
-our $VERSION = '0.134_01';
+our $VERSION = '0.135';
 
 use base qw{ Astro::Coord::ECI Exporter };
 
